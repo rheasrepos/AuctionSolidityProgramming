a9e6db60896ef7c09cc39675940418d05aa7d5cf
rhea <rhea0866@gmail.com>
2021-11-11 23:41:28 -0800

    Update Auction.sol

--- Auction.sol
+++ Auction.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.8.9 <0.9.0;
+import "./User.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract Auction{
     uint256 public buyNowPrice;
@@ -11,58 +13,57 @@ contract Auction{
 	bool public isAlive;
     User public seller; //owner of the Auction
 	mapping(User => uint256) private highestBid; //should always stay length 1 dictionary - string is the username of the highest Bid and Integer is the amount
+    IERC721 public immutable nft;
+    uint256 public immutable nftID;
  
- 
-    constructor(){
+    constructor(address realNft, uint256 realNftID){
 		//closingTimer = 43200; //30 days worth of minutes
 		isAlive = true;//not really adding timer functionality rn
-		User defaultUser = new User( "default", "default", 0.0);
-		highestBid = new mapping(User => uint256);
-		highestBid.put(defaultUser, 0.0);
+		User defaultUser = new User();
+	//	highestBid = new mapping(User => uint256);
+		highestBid.push(defaultUser, 0);
 		seller = User();
+		nft = realNft;
+		nftID = realNftID;
 	}
 	
 
 
-	function makeBid(Double amount, User person) public aliveAuction{
-		bidMade = true;
+	function makeBid(uint256 amount, User person) public aliveAuction{
+	    	bidMade = true;
 		
-		if(amount < getHighestBidAmount()) {
-			System.out.println("bid not allowed");
-		}
-		else if(amount > person.getBalance()) {
-			System.out.println("bid not allowed");
-			
-		}
-		else {
-			highestBid.put(person, amount);
-			highestBid.remove(getHighestBidder());
-
-		}
+			require(!(amount > person.getBalance()) || !(amount < getHighestBidAmount()), highestBid.put(person, amount));
+		
+		    if(highestBid.length() > 1){
+		    	highestBid.remove(getHighestBidder());
+		    }
+		
+	
 
 	}
 
 	function  pullHighestBid ()  public onlyHighestBidder aliveAuction{ // does not go to the second highest bidder, clears the auction and extends time instead
 		
-			highestBid.remove(person);
+		highestBid.remove(1);
 		
-		this.extendTime(5.0);//adds 5 min
+		extendTime(5);//adds 5 min
 
 	}
 
-	function extendTime (Double addedTime) public{
+	function extendTime (uint256 addedTime) public{
 		closingTimer += addedTime;
 	}
 
-	function finishAuction() public aliveAuction{
+	function finishAuction() public aliveAuction isOwner{
 		isAlive = false;
 		User winner = getHighestBidder();
-		Double winningAmnt = getHighestBidAmount();
-
+		uint256 winningAmnt = getHighestBidAmount();
+        
 		winner.setBalance(winner.getBalance() - winningAmnt);
-		
-		System.out.println(winner.getUsername() + " has won this auction with a bid of " + winningAmnt + "");
-
+		nft.safeTransferFrom(seller, winner, nftID);
+		nft.approve(winner, nftID);
+		require(true, winner.getUsername() + " has won this auction with a bid of " + winningAmnt + "");
+        
 	}
 	
 	function getHighestBidder() public returns(User) {
@@ -73,6 +74,11 @@ contract Auction{
 		return highestBid.entrySet().iterator().next().getValue();
 	}
 	
+//	function transferFrom(User tFrom, User to, nftID) public payable{
+	   // if(isAlive == false){
+	        
+	   // }	
+//	}
 
     modifier onlyHighestBidder() {
         require(msg.sender == getHighestBidder, "Not highest bidder");
@@ -84,7 +90,14 @@ contract Auction{
         require(isAlive, "Not a live auction");
         _;
     }
+    
+    modifier isOwner(){
+        require(msg.sender == seller, "you cannot perform this acttion, you are not the seller");
+        _;
+    }
+    
 }
+
     
     
     
